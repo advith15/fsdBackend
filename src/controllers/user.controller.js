3f57cc2b5e528c4058f00e0985bcccaf3e50b7eb
advith <advith2004@gmail.com>
2025-01-14 16:32:34 +0530

    added user controller and routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,83 @@
-import { asyncHandler } from '../middlewares/asyncHandler';
+import { asyncHandler } from '../utils/asyncHandler.js';
+import { ApiError } from '../utils/ApiError.js';
+import { User } from '../models/user.model.js';
+import { uploadOnCloudinary } from '../utils/cloudinary.js';
+import { upload } from '../middlewares/multer.middleware.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "Okay"
-    });
+    //get user details
+    //validation
+    //check whether user already exists from email
+    //check for images, check for avatar
+    //if present, send to cloudinary
+    //create user object - save to db
+    //remove password and refresh token from response 
+    //check for user creation 
+    //return respose 
+    
+    const {fullName, email, username, password} = req.body
+    console.log(email);
+
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{ email }, { username }]
+    })
+    
+    if(existedUser){
+        throw new ApiError(409, "User already exists")
+    }
+    //console.log(req.files)
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+    
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if(!avatar){
+        throw new ApiError(500, "Image upload failed")
+    }   
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser= await User.findById(user._id).select("-password -refreshToken");
+
+    if(!createdUser){
+        throw new ApiError(500, "User creation failed")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User created successfully")
+    )
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+    //get user details
+    //validation of username email
+});
+
+export { 
+    registerUser,
+    loginUser 
+};
--- src/index.js
+++ src/index.js
@@ -1,57 +1,22 @@
-import dotenv from "dotenv";
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import { error, log } from "console";
-
+import {app} from './app.js'
 dotenv.config({
-    path: "./env"
-});
+    path: './.env'
+})
+
+
 
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
-.catch((err)  => {
-    console.log("connection failed");
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import express from "express";
-const app = express();
-/*
-(async ()=>{
-    try {
-       await mongoose.connect(`${process.env.MONGO_URI}/DB_NAME}`)
-       app.on("error",(error)=>{
-        console.log("ERROR: ", error);
-        throw err
-       })
-       app.listen(process.env.PORT,()=>{
-           console.log(`Server is running on port ${process.env.PORT}`);
-       })
-    } catch (error) {
-        console.log("ERROR: ", error);
-        throw err
-    }
-})
-*/
